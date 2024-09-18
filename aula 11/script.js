dd984b141f2f80d493853ea3b3c4d7f98ad2c7a4
nanny-giovanni <>
2024-09-18 09:59:32 -0300

    aula 11

---
+++ aula 11/script.js
@@ -0,0 +1,6 @@
+const NUMERO = document.querySelector('div.numero span')
+const BTN_ZERAR = document.querySelector('.zerar')
+const BTN_VOLTAR = document.querySelector('.voltar')
+const BTN_AVANCAR = document.querySelector('.avancar')
+
+let contador = 0
