0945d88583e2935af004fae461528f2ace0ab5fa
mateusperin14 <165189258+Justiceiro92@users.noreply.github.com>
2024-09-25 10:04:00 -0300

    aula 12

--- aula 11/script.js
+++ aula 11/script.js
@@ -4,3 +4,26 @@ const BTN_VOLTAR = document.querySelector('.voltar')
 const BTN_AVANCAR = document.querySelector('.avancar')
 
 let contador = 0
+
+BTN_ZERAR.addEventListener('click', zerar)
+
+function zerar() {
+    contador = 0
+    NUMERO.innerText = contador
+}
+
+BTN_AVANCAR.addEventListener('click', avancar)
+ 
+function avancar() {
+    if (contador < 99999)
+    contador += 1
+    NUMERO.innerText = contador
+}
+
+BTN_VOLTAR.addEventListener('click', voltar)
+
+function voltar() {
+    if (contador > -99999)
+    contador -= 1
+    NUMERO.innerText = contador
+}
---
+++ aula 12/script.js
@@ -0,0 +1,32 @@
+/* PASSO 1 */
+const ENTRADA = document.querySelector('input')
+const BOTAO = document.querySelector('button')
+const FUNDO = document.querySelector('body')
+
+/* PASSO 2 */
+BOTAO.addEventListener ('click', alterar_fundo)
+
+// PASSO 3
+function alterar_fundo() {
+    let cor = ENTRADA.value
+
+    if (cor == 'vermelho') {
+        FUNDO.style.backgroundColor = 'red'
+    }
+
+    if (cor == 'azul') {
+        FUNDO.style.backgroundColor = 'blue'
+    }
+
+    if (cor == 'verde') {
+        FUNDO.style.backgroundColor = 'green'
+    }
+
+    if (cor == 'laranja') {
+        FUNDO.style.backgroundColor = 'orange'
+    }
+
+    if (cor == 'branco') {
+        FUNDO.style.backgroundColor = 'white'
+    }
+}
