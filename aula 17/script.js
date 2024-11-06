e832d77064337de8e53ab9387cc6345d5ed489d6
eduardocologni <eduardocologni@gmail.com>
2024-11-06 09:59:17 -0300

    aula 17

--- aula 17/script.js
+++ aula 17/script.js
@@ -4,6 +4,12 @@ const TAREFAS_CONTAINER = document.querySelector('.tarefas')
 
 const MEMORIA_CACHE = JSON.parse(localStorage.getItem('tarefas'))
 
+if (MEMORIA_CACHE) {
+    MEMORIA_CACHE.forEach((item)=>{
+        adicionar_tarefa(item)
+    })
+}
+
 FORMULARIO.addEventListener('submit', (event) => {
     event.preventDefault()
     adicionar_tarefa()
@@ -20,11 +26,46 @@ const TAREFAS_LISTA = document.querySelectorAll('li')
 const QUANTIDADE_TAREFAS = TAREFAS_LISTA.length
 if (entrada_texto && QUANTIDADE_TAREFAS <= 13) {
     const ELEMENTO_TAREFA = document.createElement('li')
+    if (texto && texto.completed) {
+        ELEMENTO_TAREFA.classList.add('completed')
+    }
 
     ELEMENTO_TAREFA.innerText = entrada_texto
     TAREFAS_CONTAINER.appendChild(ELEMENTO_TAREFA)
     
+    completar_tarefa(ELEMENTO_TAREFA)
+    remover_tarefa(ELEMENTO_TAREFA)
     ENTRADA.value = ''
+    atualizar_cache()
 
     }
+}
+
+function completar_tarefa(tarefa_elemento) {
+    tarefa_elemento.addEventListener('click', () => {
+        tarefa_elemento.classList.toggle('completada')
+        atualizar_cache()
+    })
+}
+
+function remover_tarefa(tarefa_elemento) {
+    tarefa_elemento.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
+        tarefa_elemento.remove()
+        atualizar_cache()
+    })
+}
+
+function  atualizar_cache(){
+    const ITENS_TAREFAS = document.querySelectorAll('li')
+    const LISTA_TAREFAS = []
+    ITENS_TAREFAS.forEach((elemento) => {
+        LISTA_TAREFAS.push({
+            txt: elemento.innerText,
+            completed: elemento.classList.contains('completada')
+        })
+
+    })
+    localStorage.setItem('tarefas', JSON.stringify(LISTA_TAREFAS))
+
 }
